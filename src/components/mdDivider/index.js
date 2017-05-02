1fa22d6ad3ae7334b9985391d5c871a197e9ee78
ZiBY <yashin.ziby@gmail.com>
2017-05-03 00:07:41 +0300

    Update divider component

--- src/components/mdDivider/index.js
+++ src/components/mdDivider/index.js
@@ -1,5 +1,8 @@
 import mdDivider from './mdDivider.vue';
+import mdDividerTheme from './mdDivider.theme';
 
 export default function install(Vue) {
   Vue.component('md-divider', mdDivider);
+
+  Vue.material.styles.push(mdDividerTheme);
 }
--- src/components/mdDivider/mdDivider.vue
+++ src/components/mdDivider/mdDivider.vue
@@ -1,11 +1,14 @@
 <template>
-  <hr class="md-divider">
+  <hr :class="['md-divider', themeClass]">
 </template>
 
 <style lang="scss" src="./mdDivider.scss"></style>
 
 <script>
+  import theme from '../../core/components/mdTheme/mixin';
+
   export default {
-    name: 'md-divider'
+    name: 'md-divider',
+    mixins: [theme]
   };
 </script>
